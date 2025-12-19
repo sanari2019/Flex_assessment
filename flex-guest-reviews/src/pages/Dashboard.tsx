d8cdc229cd19706f3954eb934a30b1ee7e45414f
Samuel-Anari <samachoo2010@icloud.com>
2025-12-19 02:09:55 +0000

    Flex Living Reviews Dashboard 0209

--- flex-guest-reviews/src/pages/Dashboard.tsx
+++ flex-guest-reviews/src/pages/Dashboard.tsx
@@ -37,7 +37,7 @@ const initialFilters: FilterState = {
   listingId: 'all',
   dateRange: undefined,
 };
-
+//filter by loc
 const locationFilters = ['london', 'paris', 'algiers'];
 
 export default function Dashboard() {
